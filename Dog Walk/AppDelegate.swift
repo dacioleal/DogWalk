52957de391eb58e510e5bf8604b6b76826667f6c
dacioleal <dacioleal@gmail.com>
2015-07-22 21:57:18 +0200

    First commit

---
+++ Dog Walk/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Dog Walk
+//
+//  Created by Dacio Leal on 7/22/15.
+//  Copyright (c) 2015 Dacio Leal. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+  var window: UIWindow?
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
