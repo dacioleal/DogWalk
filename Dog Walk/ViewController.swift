a71eaf52c782ca7daea385f81873679c40d0bcf5
Dacio Leal <dacioleal@gmail.com>
2015-07-23 21:06:20 +0200

    Add button action

--- Dog Walk/ViewController.swift
+++ Dog Walk/ViewController.swift
@@ -88,7 +88,23 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     @IBAction func add(sender: AnyObject) {
-        //walks.append(NSDate())
+        
+        let walkEntity = NSEntityDescription.entityForName("Walk", inManagedObjectContext: managedContext)
+        let walk = Walk(entity: walkEntity!, insertIntoManagedObjectContext: managedContext)
+        
+        walk.date = NSDate()
+        
+        var walks = currentDog.walks.mutableCopy() as! NSMutableOrderedSet
+        walks.addObject(walk)
+        
+        currentDog.walks = walks.copy() as! NSOrderedSet
+        
+        var error : NSError?
+        
+        if !managedContext.save(&error) {
+            println("Could not save: \(error), \(error!.userInfo)")
+        }
+        
         tableView.reloadData()
     }
     
